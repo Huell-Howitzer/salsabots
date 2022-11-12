acf3b0f01e2c4d9d6cef76a57dc82c9f800533bd
Ryan M Howell <rmhowell@protonmail.com>
2022-11-12 13:21:52 -0800

    REFACTOR main.py technically contained a client.

--- client.py
+++ client.py
@@ -1,8 +1,11 @@
 # bot.py
 import os
+import random
 
 import discord
 from dotenv import load_dotenv
+
+from discord.ext import commands
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -16,13 +19,16 @@ async def on_ready():
     for guild in client.guilds:
         if guild.name == GUILD:
             break
-
     print(
-        f'{client.user} is connected to {guild.name}(id: {guild.id})'
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
     )
-
-    members = '\n - '.join([member.name for member in guild.members])
-    print(f'Guild Members:\n - {members}')
+@client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Hi {member.name}, welcome to my Discord server!'
+    )
 
 @client.event
 async def on_message(message):
@@ -41,6 +47,13 @@ async def on_message(message):
     if message.content == '99!':
         response = random.choice(brooklyn_99_quotes)
         await message.channel.send(response)
+    elif message.content == 'raise-exception':
+        raise discord.DiscordException
 
+# @client.event
+# async def on_error(event, *args, **kwargs):
+#     with open('err.log', 'a') as f:
+#         f.write(f'Unhandled message: {args[0]}\n')
+#         else:
+#             raise
 client.run(TOKEN)
-
